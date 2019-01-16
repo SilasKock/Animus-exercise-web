5b9a4a16593530ee644afc8b20bb38e5cca6209b
unknown <C:\Users\Silas\AppData\Roaming\The Bat!>
2019-01-16 18:48:02 +0100

    Created a a forms without submit logic.

--- src/app/apartment-create/apartment-create.component.ts
+++ src/app/apartment-create/apartment-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApartmentDTO} from '../apartment-dto';
 
 @Component({
   selector: 'app-apartment-create',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApartmentCreateComponent implements OnInit {
 
+  // Initialize empty model 
+  model = new ApartmentDTO(null,"","",null,"","","");
+  submitted = false;
+
+  // TODO: submit form to api
+  onSubmit() { this.submitted = true}
+  get diagnostic() { return JSON.stringify(this.model); }
   constructor() { }
 
   ngOnInit() {
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,7 @@ export class DataService {
   // Get Apartments
   // Currently mock
   getApartments(){
+    // Use HttpClientMpdule to receive data from api
     return [{ "id": 1, "einzugsdatum": "01.03.2019", "street": "Richtweg", "plz": "22880", "ort": "Wedel", "Land": "Deutschland", "email": "mock@web.de"}, { "id": 2, "einzugsdatum": "01.06.2019", "street": "Waldstraße", "plz": "22880", "ort": "Wedel", "Land": "Deutschland", "email": "mock2@web.de"}]
-  }
-
-  getTest(myString: string){
-    return myString;
   }
 }
