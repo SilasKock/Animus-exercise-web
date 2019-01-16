6f039c6059c4e18433d6bc2b92ee7db695af9c44
unknown <C:\Users\Silas\AppData\Roaming\The Bat!>
2019-01-16 15:05:07 +0100

    Create basic components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApartmentsComponent } from './apartments/apartments.component';
+import { ApartmentCreateComponent } from './apartment-create/apartment-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApartmentsComponent,
+    ApartmentCreateComponent
   ],
   imports: [
     BrowserModule,
