12ea39ca39e69ee77aefab521084dc65e2bfd208
Silas <kock.silas@gmail.com>
2019-01-17 16:45:54 +0100

    Connected to local api

--- src/app/apartment-create/apartment-create.component.ts
+++ src/app/apartment-create/apartment-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApartmentDTO} from '../apartment-dto';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-apartment-create',
@@ -11,11 +12,14 @@ export class ApartmentCreateComponent implements OnInit {
   // Initialize empty model 
   model = new ApartmentDTO(null,"","",null,"","","");
   submitted = false;
-
+  constructor(private data: DataService) { }
   // TODO: submit form to api
-  onSubmit() { this.submitted = true}
+  onSubmit() { 
+    this.submitted = true
+    this.data.postApartment(this.model);
+  }
   get diagnostic() { return JSON.stringify(this.model); }
-  constructor() { }
+  
 
   ngOnInit() {
   }
